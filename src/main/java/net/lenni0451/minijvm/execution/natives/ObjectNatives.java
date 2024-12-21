040913aafed28f47173fcf4ee5014ec444ebbd9a
Lenni0451 <20379977+Lenni0451@users.noreply.github.com>
2024-12-21 21:49:30 +0100

    Added clone for arrays

--- src/main/java/net/lenni0451/minijvm/execution/natives/ObjectNatives.java
+++ src/main/java/net/lenni0451/minijvm/execution/natives/ObjectNatives.java
@@ -1,8 +1,11 @@
 package net.lenni0451.minijvm.execution.natives;
 
 import net.lenni0451.minijvm.ExecutionManager;
+import net.lenni0451.minijvm.object.ExecutorObject;
+import net.lenni0451.minijvm.object.types.ArrayObject;
 import net.lenni0451.minijvm.stack.StackInt;
 import net.lenni0451.minijvm.stack.StackObject;
+import net.lenni0451.minijvm.utils.ExceptionUtils;
 
 import java.util.function.Consumer;
 
@@ -17,6 +20,14 @@ public class ObjectNatives implements Consumer<ExecutionManager> {
         });
         manager.registerMethodExecutor("java/lang/Object.getClass()Ljava/lang/Class;", (executionManager, executionContext, currentClass, currentMethod, instance, arguments) -> {
             return returnValue(new StackObject(executionManager.instantiateClass(executionContext, instance.getOwner())));
+        });
+        manager.registerMethodExecutor("java/lang/Object.clone()Ljava/lang/Object;", (executionManager, executionContext, currentClass, currentMethod, instance, arguments) -> {
+            if (instance instanceof ArrayObject) {
+                ExecutorObject clone = executionManager.instantiateArray(executionContext, instance.getOwner(), ((ArrayObject) instance).getElements().clone());
+                return returnValue(new StackObject(clone));
+            } else {
+                return ExceptionUtils.newException(executionManager, executionContext, "java/lang/CloneNotSupportedException", "Object does not support cloning");
+            }
         });
     }
 
