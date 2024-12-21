abc8d471438704c4f088c5314dd0f8d9f3559d40
Lenni0451 <20379977+Lenni0451@users.noreply.github.com>
2024-12-21 20:25:08 +0100

    Use correct debug field

--- src/main/java/net/lenni0451/minijvm/execution/Executor.java
+++ src/main/java/net/lenni0451/minijvm/execution/Executor.java
@@ -11,10 +11,8 @@ import org.objectweb.asm.tree.MethodNode;
 
 public class Executor {
 
-    private static final boolean DEBUG = true;
-
     public static ExecutionResult execute(final ExecutionManager executionManager, final ExecutionContext executionContext, final ExecutorClass currentClass, final MethodNode currentMethod, final ExecutorObject instance, final StackElement... arguments) {
-        if (DEBUG) {
+        if (ExecutionManager.DEBUG) {
             System.out.println("Invoking method: " + currentClass.getClassNode().name + " " + currentMethod.name + currentMethod.desc);
         }
         boolean isStatic = Modifiers.has(currentMethod.access, Opcodes.ACC_STATIC);
@@ -28,7 +26,7 @@ public class Executor {
         MethodExecutor methodExecutor = executionManager.getMethodExecutor(executionContext, currentClass.getClassNode().name, currentMethod);
         ExecutionResult result = methodExecutor.execute(executionManager, executionContext, currentClass, currentMethod, instance, arguments);
         executionContext.popStackFrame();
-        if (DEBUG) {
+        if (ExecutionManager.DEBUG) {
             System.out.println("----- Finished " + currentClass.getClassNode().name + " " + currentMethod.name + currentMethod.desc + " execution with result " + result + " -----");
         }
         return result;
