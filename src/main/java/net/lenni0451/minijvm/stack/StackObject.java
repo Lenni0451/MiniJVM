0e654bbbb446b8b83733b0f0c32762869d7ac2fc
Lenni0451 <20379977+Lenni0451@users.noreply.github.com>
2025-01-02 20:01:54 +0100

    Make sure only the null object can be null

--- src/main/java/net/lenni0451/minijvm/stack/StackObject.java
+++ src/main/java/net/lenni0451/minijvm/stack/StackObject.java
@@ -6,6 +6,10 @@ public record StackObject(ExecutorObject value) implements StackElement {
 
     public static StackObject NULL = new StackObject(null);
 
+    public StackObject {
+        if (value == null && NULL != null) throw new IllegalArgumentException("Value cannot be null");
+    }
+
     @Override
     public int size() {
         return 1;
